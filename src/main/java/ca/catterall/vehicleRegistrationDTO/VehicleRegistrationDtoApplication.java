6f0979eb045034fa6be77d9afc16f92e5e4162e2
Nigel Catterall <nigel@catterall.ca>
2020-12-03 06:50:03 -0800

    Project Start
    Initial Framework Setup

---
+++ src/main/java/ca/catterall/vehicleRegistrationDTO/VehicleRegistrationDtoApplication.java
@@ -0,0 +1,13 @@
+package ca.catterall.vehicleRegistrationDTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegistrationDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRegistrationDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/catterall/vehicleRegistrationDTO/VehicleRegistrationDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.catterall.vehicleRegistrationDTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegistrationDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
